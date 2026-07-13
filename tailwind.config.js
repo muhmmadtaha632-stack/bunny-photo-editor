d390834b35a381e1308df8ec03cfe4cf032954e4
muhmmadtaha632-stack <muhmmadtaha632@gmail.com>
2026-07-14 02:52:17 +0500

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        pink: {
+          50: '#fdf2f8',
+          100: '#fce7f3',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
